32e663cf683696863bab18ca74f36ea9133f2d50
Jared Still <jkstill@gmail.com>
2024-05-07 01:41:05 -0700

    kills jobrun for all jkstill

--- kill-jr.sh
+++ kill-jr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ps -o pid,cmd | grep -E './[j]obrun.pl'| awk '{ print $1 }' | xargs -n 1 kill -9
+ps -u jkstill -o pid,cmd | grep -E './[j]obrun.pl'| awk '{ print $1 }' | xargs -n 1 kill -9
 
 
