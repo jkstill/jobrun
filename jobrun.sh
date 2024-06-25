d9794dd13fcb322c4cc9fd41e4a323aaddaebcb8
Jared Still <jkstill@gmail.com>
2024-06-24 19:19:02 -0700

    WIP bash version

--- jobrun.sh
+++ jobrun.sh
@@ -9,7 +9,53 @@ scriptHome=$(dirname -- "$( readlink -f -- "$0"; )")
 
 cd $scriptHome || { echo "could not cd $scriptHome"; exit 1; }
 
-fileName="$1"
+logDir='logs';
+
+mkdir -p $logDir
+
+logFile=$logDir/jobrun-sh-$(date +%Y-%m-%d_%H-%M-%S).log
+
+# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
+# process substitution
+# clear/recreate the logfile
+> $logFile
+exec 1> >(tee -ia $logFile)
+tee01PID=$!
+exec 2> >(tee -ia $logFile >&2)
+tee02PID=$!
+
+help () {
+	echo
+	echo $0
+	cat <<-EOF
+
+
+  -j  jobs config file - default jobs.conf
+  -r  jobrun config file - default jobrun.conf
+  -m  max concurrent jobs - default 5
+  -d  debug on
+  -h  help
+
+EOF
+}
+
+declare maxConcurrentJobs=5
+declare jobrunConfigFile=jobrun.conf
+declare jobsConfigfile=jobs.conf
+declare DEBUG='N'
+
+while getopts j:r:m:dhz arg
+do
+	case $arg in
+		j) jobsConfigfile=$OPTARG;;
+		r) jobrunConfigfile=$OPTARG;;
+		m) maxConcurrentJobs=$OPTARG;;
+		d) DEBUG='Y';;
+		hz) help; exit 0;;
+		*) help; exit 1;;
+
+	esac
+done
 
 set -u
 
@@ -20,7 +66,6 @@ declare -A runningPIDS
 declare -A completedJobs
 declare intervalSeconds=3
 
-declare maxConcurrentJobs=5
 declare numberJobsRunning
 declare pidFileDir=pidfiles
 
@@ -34,6 +79,23 @@ onTerm () {
 }
 
 trap onTerm SIGV TERM INT
+
+banner () {
+	echo
+	echo '############################################################'
+	echo "## $@ "
+	echo '############################################################'
+	echo
+}
+
+subBanner () {
+	echo
+	echo '   ============================================================'
+	echo "   == $@ "
+	echo '   ============================================================'
+	echo
+}
+
 
 fileIsReadable () {
 	if [[ -r "$1" ]]; then
@@ -57,8 +119,10 @@ getKV () {
 	if [[ $RC -eq 0 ]]; then
 		while IFS=: read key value
 		do
-			#echo "getKV - key: $key  value: $value" 1>&2
-			#echo "$arrayName['$key']"="'$value'"
+			if [[ $DEBUG == 'Y' ]]; then
+				echo "getKV - key: $key  value: $value" 1>&2
+				echo "$arrayName['$key']"="'$value'"
+			fi
 			eval "$arrayName['$key']"="'$value'"
 		done < <(grep -Ev '^\s*$|^\s*#' $fileName) 
 	else
@@ -94,21 +158,20 @@ showKV () {
 declare -A jobsConf
 declare -A jobrunConf
 
+banner "getKV jobrun.conf"
 getKV jobrunConf jobrun.conf
-getKV jobsConf jobs.conf
-
-echo '### jobrun.conf ###'
 showKV jobrunConf
 
-echo '### jobs.conf ###'
+banner "getKV jobs.conf"
+getKV jobsConf jobs.conf
 showKV jobsConf
 
-for key in "${!jobsConf[@]}"
+declare -a jobKeys ##="${!jobsConf[@]}"
+for v in ${!jobsConf[@]}
 do
-	echo "key: $key   val: ${jobsConf[$key]}"
+	jobKeys+=($v)
 done
 
-declare -a jobKeys=${!jobsConf[@]}
 
 #maxConcurrentJobs=5
 numberJobsRunning=0
@@ -116,26 +179,11 @@ numberJobsRunning=0
 
 userName=$(id -un)
 
-for jobKey in ${jobKeys[@]}
-do
-	echo jobkey: $jobKey
+pidCleanup () {
 
-	if [[ ${#runningJobs[@]} -lt $maxConcurrentJobs ]]; then
+	local -a pidsToRemove
 
-		(( numberJobsRunning++ ))
-		runningJobs[$jobKey]=${jobsConf[$jobKey]}
-		exec ${jobsConf[$jobKey]} &
-		childPID=$!
-		runningPIDS[$childPID]=$jobKey
-		mkdir -p $pidFileDir
-		ps -p $childPID -o pid,ppid,cmd > $pidFileDir/${childPID}.pid
-		unset jobsConf[$jobKey]
-		echo "Continuing"
-		continue
-
-	fi
-	
-	echo "Checking RunPIDS"
+	[[ $DEBUG == 'Y' ]] && { showKV runningPIDS; }
 
 	for runPID in ${!runningPIDS[@]} 
 	do
@@ -143,30 +191,74 @@ do
 		psOUT=$(ps --no-headers -p $runPID -o user,pid | grep "^$userName")
 		declare RC=$?
 
-		#echo "RC: $RC"
-		#echo "psOUT: $psOUT"
-		#echo "runPID: $runPID"
+		if [[ $DEBUG == 'Y' ]]; then
+			echo "RC: $RC"
+			echo "psOUT: $psOUT"
+			echo "runPID: $runPID"
+		fi
 
 		timestamp="$(getTimestamp)"
 
 		if [[ $RC -eq 0 ]]; then
 			echo "running PID Job: ${runningPIDS[$runPID]}"
-			declare currJob=${runningPIDS[$runPID]}
-			unset runningJobs[$currJob]
 			echo "$timestamp - still running "  >> $pidFileDir/${runPID}.pid 
-			(( numberJobsRunning-- ))
 		else
 			echo "$timestamp - finished "  >> $pidFileDir/${runPID}.pid 
+			declare currJob=${runningPIDS[$runPID]}
+			unset runningJobs[$currJob]
+			(( numberJobsRunning-- ))
 		fi
 	done
 
-	[[ ${#jobsConf[@]} -lt 1 ]] && { break; }
+	return
+}
+
+mkdir -p $pidFileDir
+
+while :
+do
+
+	if [[ ${#runningJobs[@]} -lt $maxConcurrentJobs ]]; then
+
+		echo "jobKeys count: ${#jobKeys[@]}"
+
+		jobKey="${jobKeys[0]}"
+		[[ -z $jobKey ]] && { break; }
+
+		echo jobkey: $jobKey
+		echo "jobsConf count: ${#jobsConf[@]}"
+
+		jobKeys=(${jobKeys[@]:1}) # shift array
+		(( numberJobsRunning++ ))
+		runningJobs[$jobKey]=${jobsConf[$jobKey]}
+
+		echo "run: ${jobsConf[$jobKey]}"
+		exec ${jobsConf[$jobKey]} &
+		childPID=$!
+		runningPIDS[$childPID]=$jobKey
+
+		ps -p $childPID -o pid,ppid,cmd > $pidFileDir/${childPID}.pid
+		unset jobsConf[$jobKey]
+		echo "Continuing"
+		continue
+
+	fi
+
+	echo "Checking RunPIDS"
+	pidCleanup
+
+	#[[ ${#jobsConf[@]} -lt 0 ]] && { break; }
 
 	echo "sleep $intervalSeconds"
 	sleep $intervalSeconds
 
 done
 
+for $pi
+
+pidCleanup
+
+kill $tee01PID $tee02PID
 wait
 
 echo "runningJobs:"
@@ -175,15 +267,8 @@ showKV runningJobs
 echo "jobsConf:"
 showKV jobsConf
 
-
-
-
-
-
-
-
-
-
+echo "jobKeys:"
+showKV jobKeys
 
 
 
